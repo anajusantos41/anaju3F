ff165a75a5d22868b5ffc56c53e6d5e90cc0e11c
ana julia dos santos <110939397+anajusantos41@users.noreply.github.com>
2024-06-14 08:55:03 -0300

    14/06/2024

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 const botoes = document.querySelectorAll(".botao");
 const conteudo = document.querySelectorAll(".aba-conteudo")
 const contador = document.querySelectorAll(".contador")
+const tempoObjetivo0 =new Date(2024/08/13/08/56)
+const tempoObjetivo1 =new Date(2024/09/16/08/56)
+const tempoObjetivo2 =new Date(2024/05/14/08/56)
+const tempoObjetivo3 =new Date(2024/03/14/08/56)
+contador[0].textContent="olha"
+contador[1].textContent="olha"
+contador[2].textContent="olha"
+contador[3].textContent="lins"
 for (let i = 0 ; i < botoes.length ; i++){
     botoes[i].onclick = function (){
         for (let j = 0; j < botoes.length ; j++){
